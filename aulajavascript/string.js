6e27ea44199cbea5cec4432f82c9891838c7148c
Hannah Summer <hannahlaisa@gmail.com>
2022-08-31 17:22:01 -0300

    aulas js

--- aulajavascript/string.js
+++ aulajavascript/string.js
@@ -1,25 +1,29 @@
-const escola = "8 5 10 9"
+const escola = "Hannah Laisa de Melo" 
+// As funções abaixo trazem o resultado de acordo com a tabela ASCII
+console.log(escola.charCodeAt(5))
+console.log(escola.codePointAt(5))
+// Retorna o tamanho da string
 console.log(escola.length)
 console.log(escola.charAt(4))
 console.log(escola.charAt(5))
 console.log(escola[5])
-console.log(escola.charCodeAt(3))
 console.log(escola.indexOf('-'))
-var notas = escola.split(' ')
-console.log(notas)
-console.log((+notas[0] + +notas[1] + +notas[2] + +notas[3]) / 4)
+
 console.log(escola.substring(4))
 console.log(escola.substring(0, 6))
-console.log(escola.split("Carolina"))
-var nomes = escola.split(" ")
+console.log(escola.split("i"))
+var nomes = escola.split("n")
 nomes[2] = "Summer"
 console.log(nomes)
 console.log(nomes[0],nomes[2], nomes[4]) 
 console.log(escola.split(" ")[4])
 console.log(escola.split(" ")[4])
-
+// Essa é uma forma de concatenar variaveis/arrays e formar uma frase
 console.log('Escola '.concat(escola).concat("!"))
-console.log('Escola ' + escola +"!") 
+console.log('Escola ' + escola +"!")
+// usando o  constructor
+var frase = "Hello word!"
+console.log(frase.constructor)
 
 console.log(escola.replace('10', '5'))
 var pessoa = [{nome : 'Hannah' , idade : 14 , peso: 42}, {nome : 'Henning' , idade : 38}]
